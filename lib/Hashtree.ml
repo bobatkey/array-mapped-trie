ec5021ade6b921c8573e79b4b4977b0bb90a6145
Robert Atkey <bob.atkey@gmail.com>
2013-02-27 09:14:21 +0000

    Switch to using a record inside Hashtree

--- lib/Hashtree.ml
+++ lib/Hashtree.ml
@@ -2,18 +2,9 @@ type 'a item =
   | Subtree of int * 'a item array
   | Value   of int * 'a
 
-type 'a t = int * 'a item array
-
-(* FIXME: would be nice if the array was packed:
-
-   - type 'a t = int * Obj.t array
-
-   - (map,t)
-
-   - t.(3*offset) contains value or submap tag
-   - t.(3*offset+1) contains either 'key' or 'map'
-   - t.(3*offset+2) contains either pointer to 'a or pointer to subtree
-*)
+type 'a t = { map   : int
+            ; items : 'a item array
+            }
 
 (* http://en.wikipedia.org/wiki/Hamming_weight#Language_support *)
 let ctpop map =
@@ -29,11 +20,11 @@ let ctpop map =
   let map = map + map lsr 32 in
   map land 0x7f
 
-let empty = 0, [| |]
+let empty = { map = 0; items = [| |] }
 
-let is_empty (map,_) = map = 0
+let is_empty {map} = map = 0
 
-let find key (map,t) =
+let find key {map;items=t} =
   let rec search skey map t =
     let bottom5 = skey land 0x1f in
     if map land (1 lsl bottom5) = 0 then
@@ -47,90 +38,104 @@ let find key (map,t) =
   in
   search key map t
 
-let remove key (map,t) =
+let remove key {map;items=t} =
   let rec remove skey map t =
     let bottom5 = skey land 0x1f in
     if map land (1 lsl bottom5) = 0 then
-      map, t
+      {map;items=t}
     else
       let offset = ctpop (map land ((1 lsl bottom5) - 1)) in
       match Array.unsafe_get t offset with
         | Value (key', _) when key = key' ->
-          map land (lnot (1 lsl bottom5)),
-          Array.init (ctpop map - 1) begin fun i ->
-            if i < offset then Array.unsafe_get t i
-            else Array.unsafe_get t (i+1)
-          end
+          { map   = map land (lnot (1 lsl bottom5))
+          ; items =
+              Array.init (Array.length t - 1) begin fun i ->
+                if i < offset then Array.unsafe_get t i
+                else Array.unsafe_get t (i+1)
+              end
+          }
         | Value _ ->
-          map, t
+          {map;items=t}
         | Subtree (submap, subt) ->
-          let submap, subt = remove (skey lsr 5) submap subt in
+          let {map=submap;items=subt} = remove (skey lsr 5) submap subt in
           if submap = 0 then
-            map land (lnot (1 lsl bottom5)),
-            Array.init (ctpop map - 1) begin fun i ->
-              if i < offset then Array.unsafe_get t i
-              else Array.unsafe_get t (i+1)
-            end
+            { map   = map land (lnot (1 lsl bottom5))
+            ; items =
+                Array.init (Array.length t - 1) begin fun i ->
+                  if i < offset then Array.unsafe_get t i
+                  else Array.unsafe_get t (i+1)
+                end
+            }
           else match subt with
             | [| Value (key', v') |] ->
               let t' = Array.copy t in
               Array.unsafe_set t' offset (Value (key', v'));
-              map, t'
+              {map;items=t'}
             | _ ->
               let t' = Array.copy t in
               Array.unsafe_set t' offset (Subtree (submap, subt));
-              map, t'
+              {map;items=t'}
   in
   remove key map t
 
-let rec make_subtree key1 skey1 v1 key2 skey2 v2 =
+let rec set_subtree key1 skey1 v1 key2 skey2 v2 ar i =
   let bottom5_1 = skey1 land 0x1f in
   let bottom5_2 = skey2 land 0x1f in
   if bottom5_1 = bottom5_2 then
     let skey1 = skey1 lsr 5 in
     let skey2 = skey2 lsr 5 in
-    Subtree (1 lsl bottom5_1,
-             [| make_subtree key1 skey1 v1 key2 skey2 v2 |])
+    let ar'    = [|Obj.magic 0|] in
+    Array.unsafe_set ar i (Subtree (1 lsl bottom5_1, ar'));
+    set_subtree key1 skey1 v1 key2 skey2 v2 ar' 0
   else
-    Subtree ((1 lsl bottom5_1) lor (1 lsl bottom5_2),
-             if bottom5_1 < bottom5_2 then
-               [| Value (key1, v1); Value (key2, v2) |]
-             else
-               [| Value (key2, v2); Value (key1, v1) |])
+    Array.unsafe_set ar i
+      (Subtree ((1 lsl bottom5_1) lor (1 lsl bottom5_2),
+                if bottom5_1 < bottom5_2 then
+                  [| Value (key1, v1); Value (key2, v2) |]
+                else
+                  [| Value (key2, v2); Value (key1, v1) |]))
 
-let add key v (map,t) =
+external unsafe_blit : 'a array -> int -> 'a array -> int -> int -> unit = "caml_array_blit"
+
+let add key v {map;items} =
   let rec insertion d skey map t =
     let bottom5 = skey land 0x1f in
     let offset  = ctpop (map land ((1 lsl bottom5) - 1)) in
     if map land (1 lsl bottom5) = 0 then
-      map lor (1 lsl bottom5),
-      (* FIXME: use Array.blit? *)
-      Array.init (ctpop map + 1) begin fun i ->
-        if i < offset then Array.unsafe_get t i
-        else if i = offset then Value (key, v)
-        else Array.unsafe_get t (i-1)
-      end
+      { map   = map lor (1 lsl bottom5)
+      ; items =
+          let l  = Array.length t in
+          let t' = Array.make (l + 1) (Obj.magic 0) in
+          unsafe_blit t 0 t' 0 offset;
+          Array.unsafe_set t' offset (Value (key, v));
+          unsafe_blit t offset t' (offset+1) (l - offset);
+          t'
+      (*  Array.init (ctpop map + 1) begin fun i ->
+            if i < offset then Array.unsafe_get t i
+            else if i = offset then Value (key, v)
+            else Array.unsafe_get t (i-1)
+          end*)
+      }
     else
-      let t' = Array.copy t in
+      let t' = (*Array.copy*) t in
       begin
         match Array.unsafe_get t' offset with
           | Value (key', v') when key = key' ->
             Array.unsafe_set t' offset (Value (key, v))
           | Value (key', v') ->
-            Array.unsafe_set t' offset
-              (make_subtree key (skey lsr 5) v key' (key' lsr (5*d)) v')
+            set_subtree key (skey lsr 5) v key' (key' lsr (5*d)) v' t' offset
           | Subtree (submap, subt) ->
-            let submap, subt = insertion (d+1) (skey lsr 5) submap subt in
+            let {map=submap;items=subt} = insertion (d+1) (skey lsr 5) submap subt in
             Array.unsafe_set t' offset (Subtree (submap, subt))
       end;
-      map, t'
+      {map; items=t'}
   in
-  insertion 1 key map t
+  insertion 1 key map items
 
 let singleton key v =
   add key v empty
 
-let iter f (_,t) =
+let iter f {items=t} =
   let rec visit t =
     Array.iter (function
       | Value (key, v) -> f key v
@@ -138,7 +143,7 @@ let iter f (_,t) =
   in
   visit t
 
-let fold f a (_,t) =
+let fold f a {items=t} =
   let accum = ref a in
   let rec visit t =
     Array.iter (function
--- test/test.ml
+++ test/test.ml
@@ -1,15 +1,44 @@
 open OUnit
 
+(*
+  properties:
+
+  - equal: has the same members
+
+  - forall t i j x y. i <> j -> add i x (add j y t) = add j y (add i x t)
+  - forall t i x y. add i x (add i y t) = add i x t
+  - forall t i x. remove i (add i x t) = t
+  - forall i. remove i empty = empty
+  - forall t i j x. i <> j -> remove i (add j x t) = add j x (remove i t)
+  - forall i x. singleton i x = add i x empty
+
+  - forall i x t. iter (fun j y -> 
+
+*)
+
+let to_assoc_list t =
+  List.sort (fun (i,_) (j,_) -> compare i j)
+    (Hashtree.fold (fun l i x -> (i,x)::l) [] t)
+
+let equal t1 t2 =
+  to_assoc_list t1 = to_assoc_list t2
+
 (******************************************************************************)
 let test_empty_is_empty () =
   assert_bool
     "empty_is_empty"
     (Hashtree.is_empty Hashtree.empty)
 
+let test_insert () =
+  assert_equal
+    (Hashtree.find 12 (Hashtree.add 12 'a' Hashtree.empty))
+    'a'
+
 (******************************************************************************)
 let suite =
   "hashtree tests" >:::
     [ "empty_is_empty" >:: test_empty_is_empty
+    ; "insert"         >:: test_insert
     ]
 
 let _ =
